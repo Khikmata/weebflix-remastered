a9a1453c6e6a9e0ac85481ffd9a6fd683205ae6d
Kamil <windycate03@gmail.com>
2023-06-10 20:13:22 +0300

    vercel

--- craco.config.js
+++ craco.config.js
@@ -4,12 +4,16 @@ const resolvePath = p => path.resolve(__dirname, p)
 
 module.exports = {
 	webpack: {
+		configure: {
+			entry: './src'
+		},
 		alias: {
 			'@components': resolvePath('./src/components'),
 			'@assets': resolvePath('./src/assets'),
 			'@store': resolvePath('./src/store'),
 			'@styles': resolvePath('./src/styles'),
 			'@utils': resolvePath('./src/utils'),
+			'@pages': resolvePath('./src/pages'),
 		},
 	}
 } 
